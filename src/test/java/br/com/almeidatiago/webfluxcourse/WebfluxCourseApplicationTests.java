9d3476081c2dce365fa617e5ce23b2fee70346e3
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-09-14 19:49:01 -0300

    feat: created project

---
+++ src/test/java/br/com/almeidatiago/webfluxcourse/WebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.almeidatiago.webfluxcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
