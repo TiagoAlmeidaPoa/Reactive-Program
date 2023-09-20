242bcd9d901085733662b3e8d47f3832a45d1cc0
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-09-19 21:27:02 -0300

    feat: created mapper and implemented endpoint save for user

--- src/main/java/br/com/almeidatiago/webfluxcourse/entity/UserEntity.java
+++ src/main/java/br/com/almeidatiago/webfluxcourse/entity/UserEntity.java
@@ -1,6 +1,5 @@
 package br.com.almeidatiago.webfluxcourse.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
--- src/main/java/br/com/almeidatiago/webfluxcourse/mapper/UserMapper.java
+++ src/main/java/br/com/almeidatiago/webfluxcourse/mapper/UserMapper.java
@@ -4,9 +4,16 @@ import br.com.almeidatiago.webfluxcourse.entity.UserEntity;
 import br.com.almeidatiago.webfluxcourse.model.request.UserRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import static org.mapstruct.NullValueCheckStrategy.*;
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 
 @Mapper(
-    componentModel = "spring"
+    componentModel = "spring",
+    nullValuePropertyMappingStrategy = IGNORE,
+    nullValueCheckStrategy = ALWAYS
 )
 public interface UserMapper {
     @Mapping(target = "id", ignore = true)
