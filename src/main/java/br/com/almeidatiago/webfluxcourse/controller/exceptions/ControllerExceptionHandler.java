f2e3fb57ee5eb14fcb1f34377da90e28c4a2e15e
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-09-21 20:50:41 -0300

    feat: created exception handler ValidationError and implemented validation with Jakarta validation

--- src/main/java/br/com/almeidatiago/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/almeidatiago/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
@@ -3,8 +3,10 @@ package br.com.almeidatiago.webfluxcourse.controller.exceptions;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 import static java.time.LocalDateTime.now;
@@ -28,6 +30,26 @@ public class ControllerExceptionHandler {
                         .path(request.getPath().toString())
                         .build()
                 ));
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<Mono<ValidationError>> validationError(
+        WebExchangeBindException ex, ServerHttpRequest request
+    ) {
+        ValidationError error = new ValidationError(
+            now(),
+            request.getPath().toString(),
+            BAD_REQUEST.value(),
+            "Validation Error",
+            "Error validating attributes"
+
+        );
+
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()){
+            error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(BAD_REQUEST).body(Mono.just(error));
     }
 
     private String verifyDupKeyEmail(String message) {
