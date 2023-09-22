a065f1676bbe34667f4570d6767c685d09b81493
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-09-22 19:57:33 -0300

    feat: created custom @TrimString annotation for String

--- src/main/java/br/com/almeidatiago/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/br/com/almeidatiago/webfluxcourse/model/request/UserRequest.java
@@ -1,15 +1,19 @@
 package br.com.almeidatiago.webfluxcourse.model.request;
 
+import br.com.almeidatiago.webfluxcourse.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
+    @TrimString
     @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
     @NotBlank(message = "must not be null or empty")
     String name,
+    @TrimString
     @Email(message = "Invalid email informed")
     String email,
+    @TrimString
     @Size(min = 3, max = 20, message = "must be between 3 and 20 characters")
     @NotBlank(message = "must not be null or empty")
     String password
