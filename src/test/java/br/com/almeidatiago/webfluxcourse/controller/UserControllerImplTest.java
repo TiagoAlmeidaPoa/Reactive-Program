cdf766e6ce70cbae7414ac531f22a1b834157ca3
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-10-04 21:40:10 -0300

    test: add integration test testSaveWithSuccess to endpoint save

---
+++ src/test/java/br/com/almeidatiago/webfluxcourse/controller/UserControllerImplTest.java
@@ -0,0 +1,77 @@
+package br.com.almeidatiago.webfluxcourse.controller;
+
+import br.com.almeidatiago.webfluxcourse.entity.UserEntity;
+import br.com.almeidatiago.webfluxcourse.mapper.UserMapper;
+import br.com.almeidatiago.webfluxcourse.model.request.UserRequest;
+import br.com.almeidatiago.webfluxcourse.service.UserService;
+import com.mongodb.reactivestreams.client.MongoClient;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+import static reactor.core.publisher.Mono.just;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class UserControllerImplTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private UserService service;
+
+    @MockBean
+    private UserMapper mapper;
+
+    @MockBean
+    private MongoClient mongoClient;
+
+    @Test
+    @DisplayName("Test endpoint save with success")
+    void testSaveWithSuccess() {
+        final var request = new UserRequest("Tiago", "tiago@gmail.com", "123");
+        when(service.save(any(UserRequest.class))).thenReturn(just(UserEntity.builder().build()));
+
+        webTestClient.post().uri("/users")
+            .contentType(APPLICATION_JSON)
+            .body(fromValue(request))
+            .exchange()
+            .expectStatus().isCreated();
+
+        verify(service, times(1)).save(any(UserRequest.class));
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
